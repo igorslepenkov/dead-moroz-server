8bb925aedd066805b526d1b80fc9cb011762079b
igorslepenkov <slepenkov.nii@yandex.by>
2022-11-24 10:33:47 +0300

    refactor(child-profile): move user assignment to callback, delete update_child_params, rename create_child_params into child_params, rename methods to update and create

--- app/controllers/users/child_profiles_controller.rb
+++ app/controllers/users/child_profiles_controller.rb
@@ -1,33 +1,32 @@
 class Users::ChildProfilesController < ApplicationController
-  def create_child_profile
-    user = User.find(params[:id])
-    user.create_child_profile(create_child_profile_params)
-    if user.save
-      render json: user, include: ['child_profile'], status: :ok
+  before_action :set_user
+
+  def create
+    @user.create_child_profile(child_profile_params)
+    if @user.save
+      render json: @user, include: ['child_profile'], status: :ok
     else
-      render json: { message: 'Errors have occured', errors: user.errors.full_messages }, status: :bad_request
+      render json: { message: 'Errors have occured', errors: @user.errors.full_messages }, status: :bad_request
     end
   end
 
-  def update_child_profile
-    user = User.find(params[:id])
-    if user.child_profile.update(update_child_profile_params)
-      render json: user, include: ['child_profile'], status: :ok
+  def update
+    if @user.child_profile.update(child_profile_params)
+      render json: @user, include: ['child_profile'], status: :ok
     else
-      render json: { message: 'Errors have occured', errors: user.child_profile.errors.full_messages },
+      render json: { message: 'Errors have occured', errors: @user.child_profile.errors.full_messages },
              status: :bad_request
     end
   end
 
   private
 
-  def create_child_profile_params
-    params.require(:create_child_profile).permit(:country, :city, :hobbies, :birthdate, :past_year_description,
-                                                 :good_deeds, :avatar)
+  def set_user
+    @user = User.find(params[:id])
   end
 
-  def update_child_profile_params
-    params.require(:update_child_profile).permit(:country, :city, :hobbies, :birthdate, :past_year_description,
-                                                 :good_deeds, :avatar)
+  def child_profile_params
+    params.require(:child_profile).permit(:country, :city, :hobbies, :birthdate, :past_year_description,
+                                          :good_deeds, :avatar)
   end
 end
